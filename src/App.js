1161e4829a1ce9ca237437bf47b95738d4a112de
ashdeep <ashdeepupadhyay@gmail.com>
2019-05-21 06:08:37 +0530

    changing app.js from function based component to class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+              <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,8 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
